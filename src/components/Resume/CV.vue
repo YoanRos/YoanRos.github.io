9787fc095ce69f50f86fe4b19417ed6aa7c39216
Yoan <yoanroszak@gmail.com>
2025-03-30 18:18:55 +0200

    first draft

---
+++ src/components/Resume/CV.vue
@@ -0,0 +1,14 @@
+<script setup>
+import VuePdfEmbed from 'vue-pdf-embed'
+
+// optional styles
+import 'vue-pdf-embed/dist/styles/annotationLayer.css'
+import 'vue-pdf-embed/dist/styles/textLayer.css'
+
+// either URL, Base64, binary, or document proxy
+const pdfSource = 'C:\Users\Yoan\Documents\CV et autres\porfolio\src\assets\Resume_Yoan Roszak.pdf'
+</script>
+
+<template>
+  <VuePdfEmbed annotation-layer text-layer :source="pdfSource" />
+</template>
