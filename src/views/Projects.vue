72efe1ebe5ccbba3633cabdaec5e70d9dad419e7
Yoan <yoanroszak@gmail.com>
2025-05-01 23:10:16 +0200

    fix: better tailwind integration

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -7,13 +7,27 @@ function showProject() {
 </script>
 
 <template>
-  <button class="cta" @click="showProject">
-    <img :src="'/logo_oz.png'" alt="logo Oz" />
-  </button>
-  <button class="cta" @click="showProject">
-    <img :src="'/logo_oz.png'" alt="logo Oz" />
-  </button>
-  <button class="cta" @click="showProject">
-    <img :src="'/logo_oz.png'" alt="logo Oz" />
-  </button>
+  <div class="mx-auto p-4 bg-cream min-h-screen flex-col">
+    <p class="text-4xl text-center text-soft-pink p-20 font-radiate">My Projects</p>
+    <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
+      <button class="bg-soft-pink hover:bg-light-peach rounded-lg" @click="showProject">
+        <img :src="'/logo_oz.png'" alt="logo Oz" class="w-full h-auto" />
+      </button>
+      <button class="bg-soft-pink hover:bg-light-peach rounded-lg" @click="showProject">
+        <img :src="'/logo_oz.png'" alt="logo Oz" class="w-full h-auto" />
+      </button>
+      <button class="bg-soft-pink hover:bg-light-peach rounded-lg" @click="showProject">
+        <img :src="'/logo_oz.png'" alt="logo Oz" class="w-full h-auto" />
+      </button>
+      <button class="bg-soft-pink hover:bg-light-peach rounded-lg" @click="showProject">
+        <img :src="'/logo_oz.png'" alt="logo Oz" class="w-full h-auto" />
+      </button>
+      <button class="bg-soft-pink hover:bg-light-peach rounded-lg" @click="showProject">
+        <img :src="'/logo_oz.png'" alt="logo Oz" class="w-full h-auto" />
+      </button>
+      <button class="bg-soft-pink hover:bg-light-peach rounded-lg" @click="showProject">
+        <img :src="'/logo_oz.png'" alt="logo Oz" class="w-full h-auto" />
+      </button>
+    </div>
+  </div>
 </template>
